e0e8b46779965f6a8d7b98f7b0fbb8e97881cd25
Abdullah Ansari <120268836+trulyabdullah@users.noreply.github.com>
2026-07-04 20:57:27 +0530

    sidebar-content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { Button } from "./components/Button";
-import { PlusIcon } from "./icons/PlusIcon";
-
 function App() {
 	return (
-		<div className="flex items-center justify-center">
-			<Button
-				variant={"primary"}
-				size={"sm"}
-				startIcon={<PlusIcon size="sm" />}
-				onClick={() => {
-					console.log("clicked");
-				}}
-			>
-				Add content
-			</Button>
+		<div>
+			<div className="flex">
+				<div className=" min-h-lvh bg-red-200 flex-1"></div>
+				<div className="min-h-lvh bg-blue-500 flex-3    "></div>
+			</div>
 		</div>
 	);
 }
--- src/icons/Plus.tsx
+++ src/icons/Plus.tsx
@@ -1,6 +1,6 @@
-import { IconClasses, type IconProp } from "./IconInterface";
+import { IconClasses, type IconProp } from "./Interface";
 
-export const PlusIcon = ({ size }: IconProp) => {
+export const Plus = ({ size }: IconProp) => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
